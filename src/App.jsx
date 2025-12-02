5a52e0686e2b32f6237e242581072b93025d5b24
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-12-02 16:57:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import film from './assets/Film'
+import './App.css'
+
+function App() {
+  const [select, setselect] = useState("");
+  const [filter, setfilter] = useState(film)
+  
+  useEffect(()=>{
+    if (!select.length) {
+      setfilter(film);
+    }
+
+    if (select.length) {
+      const newArray = film.filter((movie)=> movie.genre === select );
+      setfilter(newArray);
+    }
+  }, [select])
+
+  return (
+    <section className='container'>
+      <h2>Filtro Film</h2>
+      <select className="form-select" ria-label="Default select example" onChange={(event)=> setselect(event.target.value)}>
+        <option value="">seleziona il genere di Film</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+      </select>
+      <div className='row' >
+        
+        {filter.map((item, index)=> (
+          <div key={index} className='card col-3'>
+          <h3 className='card-title'>{item.title}</h3>
+          <p className='card-subtitle mb-2 text-body-secondary'>{item.genre}</p>
+          </div>
+
+        ))}
+      </div>
+
+
+    </section>
+  )
+}
+  export default App
